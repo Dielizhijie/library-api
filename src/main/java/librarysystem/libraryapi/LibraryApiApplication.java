1a46fd1ab5eccbc062dac63a056829eb38af5dd0
liuyuesen <1301913704@qq.com>
2018-09-08 19:26:50 +0800

    添加hello，可以跑了

--- src/main/java/librarysystem/libraryapi/LibraryApiApplication.java
+++ src/main/java/librarysystem/libraryapi/LibraryApiApplication.java
@@ -2,13 +2,21 @@ package librarysystem.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class}) //移除自动注入数据源
+@RestController  // 添加了一个注解
 public class LibraryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
 	}
 
-
+	@RequestMapping("/hello")
+	public String hello(){
+		return "no hello world";
+	}
 }
